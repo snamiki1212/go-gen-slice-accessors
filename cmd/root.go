1e76be02c9fd64c3c953864cfe1936ceed350912
snamiki1212 <snamiki1212@gmail.com>
2024-08-31 19:38:27 +0900

    doc: remove comments

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,6 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -57,12 +56,7 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, _ []string) {
-		fmt.Println("run")
-		fmt.Println("args", args.entity, args.slice, args.fieldNamesToExclude, args.input, args.output)
-
 		// Parse source code
 		data, err := parse(args, reader)
 		if err != nil {
@@ -83,8 +77,6 @@ to quickly create a Cobra application.`,
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
