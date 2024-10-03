7c14878a0a26531d590edf63bd16639d220f3b24
snamiki1212 <snamiki1212@gmail.com>
2024-10-03 23:43:47 +0900

    fix: update rename example

--- example/rename.go
+++ example/rename.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate go run -mod=mod github.com/snamiki1212/go-gen-slice-accessors --entity Account --slice Accounts --input rename.go --output rename_gen.go --rename=AccountID:GetAccountIDs --rename=Age:AgeList
+//go:generate go run ../. --entity Account --slice Accounts --input rename.go --output rename_gen.go --rename=AccountID:GetAccountIDs --rename=Age:AgeList
 type Account struct {
 	AccountID string
 	Age       int64
