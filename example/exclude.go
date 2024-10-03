a04dafbbfb00c3480e182f04a037195e4f47461f
snamiki1212 <snamiki1212@gmail.com>
2024-10-03 22:34:26 +0900

    fix: update generate cmd

--- example/exclude.go
+++ example/exclude.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate go run -mod=mod github.com/snamiki1212/go-gen-slice-accessors --entity Item --slice Items --input exclude.go --output exclude_gen.go --exclude=CreatedAt,UpdatedAt
+//go:generate go run ../. --entity Item --slice Items --input exclude.go --output exclude_gen.go --exclude=CreatedAt,UpdatedAt
 type Item struct {
 	ItemID    string
 	CreatedAt int64
